d2848bc82c19a7c2aaa62d94dd2bbb6f23562dbc
Ruben Eggel <rubeneggel@hotmail.com>
2021-02-18 18:49:27 -0300

    Entrega clase 11-PUG

--- rutas/api.js
+++ rutas/api.js
@@ -1,56 +1,56 @@
 const express = require('express')
 const router = express.Router();
-let matrizProductos = [{"id":1,"title":"iPhone 11 64 GB (Product)Red","price":159000,"thumbnail":"https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-001.jpg"},
-{"id":3,"title":"iPhone 12 64 GB azul","price":200000,"thumbnail":"https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-002.jpg"},
-{"id":4,"title":"iPhone XR 64 GB negro","price":139000,"thumbnail":"https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-003.jpg"},
-{"id":5,"title":"iPhone XR 64 GB negro","price":139000,"thumbnail":"https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-004.jpg"},
+let arrayProducts = [{ "id": 1, "title": "iPhone 11 64 GB (Product)Red", "price": 159000, "thumbnail": "https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-001.jpg" },
+{ "id": 3, "title": "iPhone 12 64 GB azul", "price": 200000, "thumbnail": "https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-002.jpg" },
+{ "id": 4, "title": "iPhone XR 64 GB negro", "price": 139000, "thumbnail": "https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-003.jpg" },
+{ "id": 5, "title": "iPhone XR 64 GB negro", "price": 139000, "thumbnail": "https://raw.githubusercontent.com/rubeneggel72/Entrega11-pug/main/img/img-004.jpg" },
 ]
 
 router.get('/productos/vista', (req, res) => {
-    console.log(matrizProductos)
-    res.render('index.pug', { productos: matrizProductos});
+    console.log(arrayProducts)
+    res.render('index.pug', { products: arrayProducts });
 });
 
 router.get('/productos', (req, res) => {
-    if (matrizProductos.length > 0) {
-        res.send(matrizProductos);
+    if (arrayProducts.length > 0) {
+        res.send(arrayProducts);
     }
     else {
-        res.send({ error: 'No hay productos cargados' });
+        res.send({ error: 'No hay products cargados' });
     }
 })
 
 router.get('/productos/:id', (req, res) => {
     let id = parseInt(req.params.id)
-    let idx = getIndice(id)
-    let producto = matrizProductos[idx]
-    if (producto != undefined) {
-        res.send(producto);
+    let idx = getindex(id)
+    let product = arrayProducts[idx]
+    if (product != undefined) {
+        res.send(product);
         return
     }
     else {
         res.send({ error: 'Producto no encontrado' });
     }
-    res.send(JSON.stringify(producto));
+    res.send(JSON.stringify(product));
 })
 
 router.post('/productos', (req, res) => {
     var id = 1
-    if (matrizProductos.length > 0) {
-        id = matrizProductos[matrizProductos.length - 1].id + 1
+    if (arrayProducts.length > 0) {
+        id = arrayProducts[arrayProducts.length - 1].id + 1
     }
     req.body.id = id
-    matrizProductos.push(req.body)
+    arrayProducts.push(req.body)
     res.send(req.body);
 })
 
 router.put('/productos/:id', (req, res) => {
     let id = parseInt(req.params.id)
-    let idx = getIndice(id)
-    let producto = matrizProductos[idx]
-    if (producto != undefined) {
+    let idx = getindex(id)
+    let product = arrayProducts[idx]
+    if (product != undefined) {
         req.body.id = id
-        matrizProductos[idx] = req.body
+        arrayProducts[idx] = req.body
         res.send(req.body);
     }
     else {
@@ -60,12 +60,12 @@ router.put('/productos/:id', (req, res) => {
 
 router.delete('/productos/:id', (req, res) => {
     let id = parseInt(req.params.id)
-    let idx = getIndice(id)
-    let producto = matrizProductos[idx]
-    
-    if (producto != undefined) {
-        matrizProductos.splice(idx, 1);
-        res.send(producto);
+    let idx = getindex(id)
+    let product = arrayProducts[idx]
+
+    if (product != undefined) {
+        arrayProducts.splice(idx, 1);
+        res.send(product);
         return
     }
     else {
@@ -78,14 +78,14 @@ router.delete('/productos/:id', (req, res) => {
 
 
 
-function getIndice(id) {
-    var indice = -1;
-    matrizProductos.filter(function (producto, i) {
+function getindex(id) {
+    var index = -1;
+    arrayProducts.filter(function (producto, i) {
         if (producto.id === id) {
-            indice = i;
+            index = i;
         }
     });
-    return indice;
+    return index;
 }
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -17,14 +17,12 @@ app.use('/api', require('../rutas/api'))
 
 app.use('/', router)
 
-
-
 app.listen(port, (err) => {
     if (err) throw new Error(err);
     console.log(`Servidor corriendo en puerto ${port}`);
 });
 
-// JSON productos para agregar
+// JSON products para agregar
 //{"title":"iPhone 11 64 GB (Product)Red","price":159000,"thumbnail":"a001.jpg"}
 //{"title":"iPhone 12 64 GB azul","price":200000,"thumbnail":"a002.jpg"}
 //{"title":"iPhone XR 64 GB negro","price":139000,"thumbnail":"a003.jpg"}
